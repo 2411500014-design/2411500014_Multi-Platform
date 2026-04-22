8b53fb89d9c1768b0793701893ef98b93bc491ca
2411500014-design <2411500014@mahasiswa.atmaluhur.ac.id>
2026-04-22 15:08:53 +0700

    Used

---
+++ lib/DashboardPage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    final String username = args['username']!;
+    final String password = args['password']!;
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Dashboard')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Hallo, $username! Selamat datang di Dashboard."),
+            SizedBox(height: 8),
+            Text("Username: $username"),
+            Text("Password: $password"),
+            SizedBox(height: 16),
+            ElevatedButton(
+              child: Text('Kembali ke Home'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
